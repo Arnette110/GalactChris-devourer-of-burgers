348522cd94886cdb49bd8ed0aa2ae64abfa764e9
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-02-28 19:20:18 -0700

    fixed ajax put to allow devour button to work depending on if "devour" on "more"

--- public/assets/js/jsburger.js
+++ public/assets/js/jsburger.js
@@ -1,19 +1,19 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".devourBtn").on("click", (event) => {
+  $(".devourBtn").on("click", event => {
     var id = $(event.target).data("id");
-    var newDevour = $(event.target).data("newDevour");
+    var newDevour = $(event.target).data("newdevour");
 
     var newDevourState = {
       devoured: newDevour,
     };
-    
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
+
+    // Send the PUT request
+    $.ajax("/api/burgers/id/" + id + "/isdevoured/" + (newDevour === 1 ? false : true), {
       type: "PUT",
       data: newDevourState,
     }).then(function() {
-      console.log("changed devoured to", newDevour);
+      
       // Reload the page to get the updated list
       location.reload();
     });
@@ -24,31 +24,33 @@ $(function() {
     event.preventDefault();
 
     var newBurger = {
-      burger_name: $(".bo").val().trim(),
-      devoured: 0
+      burger_name: $(".bo")
+        .val()
+        .trim(),
+      devoured: 0,
     };
-    console.log(newBurger);
-    // debugger
+ 
+    
     // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger,
     }).then(function() {
-      console.log("created new burger");
+      
       // Reload the page to get the updated list
       location.reload();
     });
   });
 
-  $("#deleteBtn").on("click", (event) => {
+  $(".delete-burger").on("click", event => {
     var id = $(event.target).data("id");
-    console.log(id);
+    
 
     // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
       type: "DELETE",
     }).then(function() {
-      console.log("deleted burger", id);
+      
       // Reload the page to get the updated list
       location.reload();
     });
