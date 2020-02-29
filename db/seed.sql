07f12b90fb3949b80ccb4151a4adc8e3fad4fff8
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-02-28 19:40:01 -0700

    edited seeds to include 3 different burgers

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Hickory", false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Hickory Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Garlic Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
